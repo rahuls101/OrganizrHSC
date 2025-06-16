032c7d80b2c50fb7660c66916ea6f04903c1d5e9
Rahul S <shankarling_ra@student.kings.edu.au>
2025-06-16 14:41:06 +1000

    Added the One-To-Many assessments relationship between the User model and the newly created Assessments model

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
 
 db = SQLAlchemy()
 
@@ -8,3 +9,15 @@ class User(db.Model):
     lastname = db.Column(db.String(25), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+
+    assessments = db.relationship('Assessment', backref='user', lazy=True) #links to the assessment model below - one to many
+
+
+class Assessment(db.Model): 
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False) 
+    subject_code = db.Column(db.String(10), nullable = False) #e.g MAT
+    title = db.Column(db.String(100), nullable = False)
+    description = db.Column(db.Text, nullable = True)
+    due_date = db.Column(db.DateTime, nullable = False)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
