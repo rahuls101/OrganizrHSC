e71110f787c1686ed0e950df401df85545e5d1d8
Rahul S <shankarling_ra@student.kings.edu.au>
2025-07-20 15:43:34 +1000

    fix for render

--- server.py
+++ server.py
@@ -537,4 +537,5 @@ def add_assessment():
     return redirect(url_for('assessments', message='Assessment added successfully', type='success'))
 
 if __name__ == '__main__': 
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
